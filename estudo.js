a4189a5138d1beb9e022c8f32edec03319ba5def
Nanda <108944840+NandaTrindade27@users.noreply.github.com>
2022-09-30 12:45:45 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,15 @@
+let tabuada = 5;
 
+function escreva(){
+    document.write("Tabuada Do" + tabuada  + "<br>")
+ document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
+ document.write(tabuada+" x  2 = "+(tabuada*2)+"<br>")
+ document.write(tabuada+" x 1 = "+(tabuada*3)+"<br>");
+ document.write(tabuada+" x  2 = "+(tabuada*4)+"<br>")
+ document.write(tabuada+" x 1 = "+(tabuada*5)+"<br>");
+ document.write(tabuada+" x  2 = "+(tabuada*6)+"<br>")
+ document.write(tabuada+" x 1 = "+(tabuada*7)+"<br>");
+ document.write(tabuada+" x  2 = "+(tabuada*8)+"<br>")
+ document.write(tabuada+" x 1 = "+(tabuada*9)+"<br>");
+ document.write(tabuada+" x  2 = "+(tabuada*10)+"<br>")
+}
