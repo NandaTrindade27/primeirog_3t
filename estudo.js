32b367fa03935a8054c54d9a3a24ec1293cd1514
Nanda <108944840+NandaTrindade27@users.noreply.github.com>
2022-10-05 11:56:44 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,15 +1,37 @@
 let tabuada = 5;
 
 function escreva(){
-    document.write("Tabuada Do" + tabuada  + "<br>")
- document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
- document.write(tabuada+" x  2 = "+(tabuada*2)+"<br>")
- document.write(tabuada+" x 1 = "+(tabuada*3)+"<br>");
- document.write(tabuada+" x  2 = "+(tabuada*4)+"<br>")
- document.write(tabuada+" x 1 = "+(tabuada*5)+"<br>");
- document.write(tabuada+" x  2 = "+(tabuada*6)+"<br>")
- document.write(tabuada+" x 1 = "+(tabuada*7)+"<br>");
- document.write(tabuada+" x  2 = "+(tabuada*8)+"<br>")
- document.write(tabuada+" x 1 = "+(tabuada*9)+"<br>");
- document.write(tabuada+" x  2 = "+(tabuada*10)+"<br>")
+    document.write("Tabuada do " + tabuada + "<br>")
+    document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada+" x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada+" x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada+" x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada+" x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada+" x 10 = "+(tabuada*10)+"<br>");
+    
+
+}
+ let lista =["Terra","Marte","Saturn,","Venus"];
+
+ function  mostralista(){
+     document.write("Tamanho da Lista" + lista.leigth + "<br>");
+     for(let i = 0; i < 5; i++){
+      document.write("Planeta" + lista[i] + "<br>");
+
+     }
+ }
+
+function repete(){
+for(let i = 1; i <= 10; i++){
+    document.write("Tabuada do =" + i + "<br>");
+    for(let  j = 1; j <=10; j++){
+        document.write(1+" x " + j + " = "+(i*j)+"<br>");
+
+    }
+}
+
 }
