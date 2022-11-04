b3d7476eaa5e1f25117e4abed6a8457082a9f6ae
Nanda <108944840+NandaTrindade27@users.noreply.github.com>
2022-11-04 12:20:25 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,16 +1,34 @@
-
 function total(){
   let c = document.getElementById("valor").value;
   let j = document.getElementById("juros").value;
   let t = document.getElementById("meses").value;
+  if(!Number(c)){
+alert("O  valor do capital deve ser um numero.");
+document.getElementById("valor").value = "";
+document.getElementById("valor").focus();
+return
+  }
+  if(!Number(j)){
+    alert("O  valor do juros deve ser um numero.");
+    document.getElementById("juros").value = "";
+    document.getElementById("juros").focus();
+    return
+      }
+      if(!Number(t)){
+        alert("O  valor do meses deve ser um numero.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return
+          }
   let r = 0;
   for(let i = 1; i <= t; i++){
       r = c * (1 + (j/100));
-      //document.write("Mês " + i + " valor: " + r +"<br>");
+     document.write("Mês " + i + " valor: " + r +"<br>");
       c = r;
   }
   document.write("Resultado: " + r);
-} 
+}
+
 
 function adicao(){
 let val1 = document.getElementById("v1").value;
@@ -47,7 +65,7 @@ function subtracao(){
         let r = Number(val1) * Number(p);
         document.getElementById("resultado").innerHTML =r;
         }
-  //document.write("Mês " + i + " valor: " + r +"<br>");
+  
 
 
 
